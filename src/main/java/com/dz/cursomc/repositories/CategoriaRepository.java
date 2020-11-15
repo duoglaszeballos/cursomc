7fec7f2c2676b9f8583c53b0d261b189a0e10b53
Duoglas Zeballos <duoglasjhoy@gmail.com>
2020-11-14 21:45:21 -0300

    Criando Repository e Service para Categoria

--- src/main/java/com/dz/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/dz/cursomc/repositories/CategoriaRepository.java
@@ -1,5 +1,11 @@
 package com.dz.cursomc.repositories;
 
-public class CategoriaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dz.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
 }
