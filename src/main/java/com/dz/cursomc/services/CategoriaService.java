a0826f4098293dc4927e7da430d75d2f3fa50cda
Duoglas Zeballos <duoglasjhoy@gmail.com>
2020-11-18 23:57:04 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/dz/cursomc/services/CategoriaService.java
+++ src/main/java/com/dz/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dz.cursomc.domain.Categoria;
+import com.dz.cursomc.dto.CategoriaDTO;
 import com.dz.cursomc.repositories.CategoriaRepository;
 import com.dz.cursomc.services.exceptions.DataIntegrityException;
 import com.dz.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
